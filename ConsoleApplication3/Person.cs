d6fed4c7d970b06b214f65856fbb8ea494fdcedc
jdomingo9 <domingojohn51@gmail.com>
2016-02-23 00:06:49 -0500

    I am done

---
+++ ConsoleApplication3/Person.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApplication3
+{
+    class Person
+    {
+        
+        public int Age = 0;
+        public string firstName;
+        public string lastName;
+        public Person Spouse;
+        public static double SumOfAllAges = 0;
+        public static int count = 0;
+
+        public void InputPersonData()      
+        {
+            System.Console.Write("What is your first name? ");
+            this.firstName = System.Console.ReadLine();
+            System.Console.Write("What is your last name? ");
+            this.lastName = System.Console.ReadLine();
+            System.Console.Write("What is your age? ");
+            this.Age = int.Parse(System.Console.ReadLine());
+            this.Spouse = null;
+            SumOfAllAges += this.Age;
+            count++;
+        }
+
+        public void SpouseInputData()  
+        {
+            this.Spouse = new Person();
+            System.Console.Write("What is your spouse's first name? ");
+            this.Spouse.firstName = System.Console.ReadLine();
+            System.Console.Write("What is your spouse's age? ");
+            this.Spouse.Age = int.Parse(System.Console.ReadLine());
+            this.Spouse.lastName = this.lastName;
+            this.Spouse.Spouse = this;
+            SumOfAllAges += this.Spouse.Age;
+            count++;
+        }
+
+        public string GetFullName()  
+        {
+            return this.firstName + " " + this.lastName;
+        }
+
+        public void PrintNameandAge()   
+        {
+            System.Console.WriteLine(this.GetFullName() + " is " + this.Age + " years old.");
+            if (this.Spouse != null)     
+            {
+                System.Console.WriteLine(this.Spouse.GetFullName() + " is " + this.Spouse.Age + " years old.");
+            }
+            else
+            {
+                System.Console.WriteLine(this.GetFullName() + " does not have a spouse.");
+            }
+        }
+    }
+}
+
+    
+
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                bool maritalStatus;
+
+
+                Person p1 = new Person();
+                System.Console.WriteLine("Person 1: ");
+
+                p1.InputPersonData();
+
+                System.Console.Write(" Are you married? True or False");
+                maritalStatus = bool.Parse(System.Console.ReadLine());
+                if (maritalStatus)
+                {
+                    p1.SpouseInputData();
+                }
+                System.Console.WriteLine();
+
+
+                Person p2 = new Person();
+                System.Console.WriteLine("Person 2: ");
+
+                p2.InputPersonData();
+
+                System.Console.Write("Are you married? True or False ");
+                maritalStatus = bool.Parse(System.Console.ReadLine());
+                if (maritalStatus)
+                {
+                    p2.SpouseInputData();
+                }
+                System.Console.WriteLine();
+
+                p1.PrintNameandAge();
+                p2.PrintNameandAge();
+                System.Console.WriteLine("Average Age: " + Person.SumOfAllAges / Person.count + " years old.");
+                System.Console.WriteLine();
+
+
+                System.Console.WriteLine();
+                System.Console.WriteLine("Press Any Key");
+                System.Console.ReadKey();
+
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
